a9845a463c0fb77a9f9c387e66d8bc7b40ce3b7c
645996795@qq.com <645996795@qq.com>
2019-03-02 20:58:56 +0800

    added files

---
+++ dellink/delink.c
@@ -0,0 +1,13 @@
+#include "malloc.h"
+#include <stdio.h>
+
+typedef struct stu_s{
+	int num;
+	struct stu_s *next;
+}stu_t;
+
+int main()
+{
+	stu_t *p, *q, *p1, *head, *qt;
+	printf("dellink\n");
+}
