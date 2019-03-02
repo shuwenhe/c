1dad6f1796012d2e9f8de608edc3fb2c7cd47489
645996795@qq.com <645996795@qq.com>
2019-03-02 22:18:54 +0800

    added files

--- dellink/delink.c
+++ dellink/delink.c
@@ -9,5 +9,11 @@ typedef struct stu_s{
 int main()
 {
 	stu_t *p, *q, *p1, *head, *qt;
+	int n, i;
+	scanf("%d\n", &n);
+	for(i = 1; i <= n; i++){
+		p = malloc(stu);
+	}
+ 
 	printf("dellink\n");
 }
